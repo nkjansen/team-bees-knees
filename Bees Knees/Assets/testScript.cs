687276f5c957a43688f2b4564794eb76f6c9f5e0
Nate12503 <nathanshussey@gmail.com>
2020-09-26 13:08:13 -0400

    Update testScript.cs

--- Bees Knees/Assets/testScript.cs
+++ Bees Knees/Assets/testScript.cs
@@ -7,7 +7,7 @@ public class testScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hi.");
     }
 
     // Update is called once per frame
