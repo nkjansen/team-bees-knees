4c397ab3fe133d5eca12ce10164dd24709dfd1bd
Nate12503 <nathanshussey@gmail.com>
2020-10-11 14:47:04 -0400

    Outline For Character Controller
    Set up a class that checks the keys that are being used. Look to possibly make this an abtract class that DD and Eubie inherit from

---
+++ Bees Knees/Assets/Scripts/Character Controllers/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    #region Controls
+    [SerializeField]
+    private KeyCode Forward = KeyCode.W;
+    [SerializeField]
+    private KeyCode Backwards = KeyCode.S;
+    [SerializeField]
+    private KeyCode Left = KeyCode.A;
+    [SerializeField]
+    private KeyCode Right = KeyCode.D;
+    [SerializeField]
+    private KeyCode Jump = KeyCode.Space;
+    #endregion
+
+    private Rigidbody rb;
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controlCheck();
+    }
+
+    private void controlCheck()
+    {
+        if (Input.GetKey(Forward))
+        {
+            Debug.Log("Moving Forward!");
+        }
+
+        if (Input.GetKey(Backwards))
+        {
+            Debug.Log("Moving Backwards!");
+        }
+
+        if (Input.GetKey(Left))
+        {
+            Debug.Log("Moving Left!");
+        }
+
+        if (Input.GetKey(Right))
+        {
+            Debug.Log("Moving Right!");
+        }
+
+        if (Input.GetKeyDown(Jump))
+        {
+            Debug.Log("Jumping!");
+        }
+    }
+}
