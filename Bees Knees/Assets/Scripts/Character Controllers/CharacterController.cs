0c24d87817d36ef689433d8c45517a5cc02047df
Nate12503 <nathanshussey@gmail.com>
2020-10-11 15:29:00 -0400

    Updated CharacterController Framework
    Set up the frame work to add the actual meat of the player controller

--- Bees Knees/Assets/Scripts/Character Controllers/CharacterController.cs
+++ Bees Knees/Assets/Scripts/Character Controllers/CharacterController.cs
@@ -21,6 +21,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 1f;
 
+
+    #region Unity Callbacks
     // Start is called before the first frame update
     void Start()
     {
@@ -32,32 +34,60 @@ public class CharacterController : MonoBehaviour
     {
         controlCheck();
     }
+    #endregion
 
     private void controlCheck()
     {
         if (Input.GetKey(Forward))
         {
-            Debug.Log("Moving Forward!");
+            moveForward();
         }
 
         if (Input.GetKey(Backwards))
         {
-            Debug.Log("Moving Backwards!");
+            moveBack();
         }
 
         if (Input.GetKey(Left))
         {
-            Debug.Log("Moving Left!");
+            moveLeft();
         }
 
         if (Input.GetKey(Right))
         {
-            Debug.Log("Moving Right!");
+            moveRight();
         }
 
         if (Input.GetKeyDown(Jump))
         {
-            Debug.Log("Jumping!");
+            jump();
         }
     }
+
+    #region Movement Methods
+    private void moveForward()
+    {
+        Debug.Log("Moving Forward!");
+    }
+
+    private void moveBack()
+    {
+        Debug.Log("Moving Backwards!");
+    }
+
+    private void moveLeft()
+    {
+        Debug.Log("Moving Left!");
+    }
+
+    private void moveRight()
+    {
+        Debug.Log("Moving Right!");
+    }
+
+    private void jump()
+    {
+        Debug.Log("Jumping!");
+    }
+    #endregion
 }
