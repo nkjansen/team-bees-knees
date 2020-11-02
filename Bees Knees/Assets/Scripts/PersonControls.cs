7d174a7c0444a5bc91d20e01092c7c4c3c0037db
jenfield99 <64561971+jenfield99@users.noreply.github.com>
2020-11-02 15:14:44 -0500

    reverting merges n stuff (pray 4 me)

--- Bees Knees/Assets/Scripts/PersonControls.cs
+++ Bees Knees/Assets/Scripts/PersonControls.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿<<<<<<< HEAD
 ﻿using System.Collections;
 using UnityEngine;
@@ -36,3 +37,42 @@ public class PersonControls : MonoBehaviour
         }
     }
 }
+=======
+﻿using System.Collections;
+using UnityEngine;
+
+public class PersonControls : MonoBehaviour
+{
+    static Animator anim;
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(Input.GetButtonDown("Pick"))
+        {
+            anim.SetTrigger("isPicking");
+        }
+
+        if(translation !=0)
+        {
+            anim.SetBool("isSwimming", true);
+        }
+        else
+        {
+            anim.SetBool("isSwimming", false);
+        }
+    }
+}
+>>>>>>> parent of 2957361... Merge branch 'dev-main' of https://github.com/Nate12503/team-bees-knees into dev-main
