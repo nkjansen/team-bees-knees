cb307d63eb7df4524506141041fdf1f4191e0285
Chuntestev72674 <62954218+Chuntestev72674@users.noreply.github.com>
2020-11-03 19:56:55 -0500

    Lilypad Puzzle Added to Main Scene

---
+++ Bee's Knees/Assets/Scripts/FallingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    private bool isFalling = false;
+    private float downSpeed = 0;
+    //private Vector3 checkpoint;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == ("player"))
+            isFalling = true;
+            //Object disappears after 4 seconds
+            Destroy(gameObject, 4);
+        //the current position of the object
+        //where the object resets to
+        //checkpoint = new Vector3(1.1f, -9.4f);
+    }
+
+    void Update()
+    {
+        if (isFalling)
+        {
+            //Acceleration of the object falling
+            //The higher the number the slower the object falls
+            downSpeed += Time.deltaTime/9770;
+            //changing the position of the object falling(down)
+            transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed, transform.position.z);
+        }
+
+        //whenever the object gets below 2 on the y axis
+        //if (transform.position.y < -15.9)
+        {
+            //object resets to the original postion
+            //transform.position = checkpoint;
+        }
+    }
+}
---
+++ Bee's Knees/Assets/Scripts/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    //drop player transform in this slot
+    [SerializeField] 
+    private Transform player; 
+    //drop respawn point gameobject in this slot
+    [SerializeField] 
+    private Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //whenever the player jumps on the water, they will respawn to the respawnPoint
+            player.transform.position = respawnPoint.transform.position;
+            Physics.SyncTransforms();
+        }
+    }
+}
