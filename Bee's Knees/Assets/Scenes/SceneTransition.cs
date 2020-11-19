a224a385b48d3013becdc03568d17c664b58a75e
Chuntestev72674 <62954218+Chuntestev72674@users.noreply.github.com>
2020-11-19 14:15:49 -0500

    Transition Script Included

---
+++ Bee's Knees/Assets/Scenes/SceneTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+    
+    void Update()
+    {
+       //Transition goes here
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(2);
+        }
+    }
+
+
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    
+   public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
