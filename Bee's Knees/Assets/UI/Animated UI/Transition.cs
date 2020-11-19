c3c51315ca048d01b6aafc3fb7cf2dafd257516e
SunnyValentine <66551747+SunnyValentine@users.noreply.github.com>
2020-11-19 14:20:24 -0800

    Update Scene transition

---
+++ Bee's Knees/Assets/UI/Animated UI/Transition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Void Update is the Trigger Point
+    // Currently this only requires player to press left mouse button  trigger scene transition
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToNextLevel();
+        }
+    }
+
+    public void FadeToNextLevel ()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("Fadeout");
+
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
